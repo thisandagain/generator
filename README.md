9d6bc394269a8d8477dd2f6b518729a757a8add8
Andrew Sliwinski <thisandagain@gmail.com>
2012-10-01 14:27:54 -0700

    Prepare for initial release

---
+++ README.md
@@ -0,0 +1,57 @@
+## Generator
+A simple project bootstrapping utility.
+
+### Installation
+```bash
+[sudo] npm install -g generator
+generate --setup
+```
+
+### Basic Use
+```bash
+generate -t module -n myAwesomeProject
+```
+
+### Creating A New Template
+Templates are simply directories with any combination of files and sub-directories found within them. To create a new template simply create a new directory within your `~/.generator` path or copy one of the default templates and modify it. Upon use, Generator will walk the template looking for any instances of `__yourVariableName__` and prompt for a value. For example, a template including this:
+
+```javascript
+/**
+ * __description__
+ *
+ * @package __name__
+ * @author __author__ <__email__>
+ */
+```
+
+Will prompt:
+```bash
+description: A description of my project
+author: Nyan Cat
+email: me@somedomain.com
+```
+
+Which will then write:
+```javascript
+/**
+ * A description of my project
+ *
+ * @package myAwesomeProject
+ * @author Nyan Cat <me@somedomain.com>
+ */
+```
+
+---
+
+### Default Templates
+Generator comes with a number of templates to get you started (additional "starter" templates are welcomed). While they are usable as-is, the whole point of Generator is make the creation and modification of custom templates easy... so go get your hands dirty and hack your own!
+`module` - Template for a standard [Node.js](http://nodejs.org/) module
+`binary` - Template for a standard [Node.js](http://nodejs.org/) CLI application
+`arduino` - Template for a basic [Arduino](http://www.arduino.cc/) project
+
+---
+
+### Testing
+```bash
+npm test
+```
---
+++ bin/index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+/**
+ * Command line interface for generator.
+ *
+ * @package generator
+ * @author Andrew Sliwinski <andrew@diy.org>
+ */
+
+/**
+ * Dependencies
+ */
+var bin     = require('commander'),
+    colors  = require('colors'),
+    package = require('../package.json');
+
+var Setup   = require('../lib/setup.js'),
+    Replica = require('../lib/replica.js'),
+    Walk    = require('../lib/walk.js');
+
+/**
+ * Defaults
+ */
+var TEMPLATE_SRC = __dirname + '/../templates';
+var TEMPLATE_DIR = process.env.HOME + '/.generator';
+function stderr (err) {
+    console.log('Error: '.red + err.red);
+    process.exit(1);
+}
+
+/**
+ * CLI definition
+ */
+bin
+    .version(package.version)
+    .usage('-n [project name] -t [template name]')
+    .option('-t, --template <template>', 'Template name (from ~/.generator')
+    .option('-n, --name <name>', 'Name of project to be generated')
+    .option('-s, --setup', 'Initializes the generator template directory')
+    .parse(process.argv);
+
+/**
+ * Setup
+ */
+if (bin.setup) {
+    new Setup(TEMPLATE_SRC, TEMPLATE_DIR, function (err) {
+        if (err) stderr(err);
+        console.log('Setup complete'.green);
+    });
+} else {
+    /**
+     * Check requirements
+     */
+    if (typeof bin.template === 'undefined' || bin.name === 'index.js') {
+        stderr('Template and project name must be specified. See --help.');
+    }
+
+    /**
+     * Copy template & process
+     */
+    new Replica(TEMPLATE_DIR+'/'+bin.template, bin.name, function (err, path) {
+        if (err) stderr(err);
+        new Walk(path, bin.name, false);
+    });
+}
