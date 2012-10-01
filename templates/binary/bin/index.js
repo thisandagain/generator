6761e84e52023f30b82cd7300830e8aded867a98
Andrew Sliwinski <thisandagain@gmail.com>
2012-10-01 14:27:15 -0700

    Added binary template

---
+++ templates/binary/bin/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+/**
+ * __description__
+ *
+ * @package __name__
+ * @author __author__ <__email__>
+ */
+
+/**
+ * Dependencies
+ */
+var _       = require('underscore'),
+    async   = require('async');
+
+/**
+ * CLI
+ */
+console.log('Hello world!');
+process.exit();
